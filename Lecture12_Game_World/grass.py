bf088a32bbc6040a0e156b778b8700f6e6a28832
ehdxorhrl <leedj6477@naver.com>
2023-10-18 18:44:56 +0900

    Drill10 제출(2020182029 이동재)
    Drill10 제출

---
+++ Lecture12_Game_World/grass.py
@@ -0,0 +1,16 @@
+from pico2d import load_image
+
+
+class Grass:
+    def __init__(self):
+        self.y = 0
+        self.image = load_image('grass.png')
+
+    def draw(self):
+        self.image.draw(400, self.y)
+
+    def update(self):
+        pass
+
+    def set_position(self, _y):
+        self.y = _y
